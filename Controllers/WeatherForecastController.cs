bf29b43ce17fd02da9254c03f99dd0605a4e56d9
Lucian Masca <lucianm0607@icloud.com>
2021-09-01 12:37:46 +0300

    Created Method ConvertResponseToWeatherForecast

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RestSharp;
 
 namespace AspNetSandbox.Controllers
 {
@@ -26,6 +27,27 @@ namespace AspNetSandbox.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            var client = new RestClient("https://api.openweathermap.org/data/2.5/onecall?lat=45.657974&lon=25.601198&exclude=hourly,minutely&appid=1637fc62cd976a8699f16ac5f7d9b92f");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+
+            return ConvertResponseToWeatherForecast(response.Content);
+
+            var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //    //https://api.openweathermap.org/data/2.5/onecall?lat=45.657974&lon=25.601198&exclude=hourly,minutely&appid=1637fc62cd976a8699f16ac5f7d9b92f
+        }
+    
+
+        private IEnumerable<WeatherForecast> ConvertResponseToWeatherForecast(string content)
+        {
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
@@ -34,7 +56,6 @@ namespace AspNetSandbox.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
-            //https://api.openweathermap.org/data/2.5/onecall?lat=45.657974&lon=25.601198&exclude=hourly,minutely&appid=1637fc62cd976a8699f16ac5f7d9b92f
-        }
+    }
     }
 }
