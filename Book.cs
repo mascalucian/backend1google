b3b12d12236ca553dd151d44f488a97df0659c5c
Lucian Masca <lucianm0607@icloud.com>
2021-09-03 10:33:33 +0300

    Added Book class and controller

---
+++ Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetSandbox
+{
+    public class Book
+    {
+        public string Id { get; set; }
+        public string title { get; set; }
+
+        public string Author { get; set; }
+        public string Language { get; set; }
+
+
+    }
+}
