186a2a7644117c7a482af2d9748b5e3543331280
Lucian Masca <lucianm0607@icloud.com>
2021-09-08 19:22:02 +0300

    Created Books service to separate functions from Controller

---
+++ BooksService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetSandbox
+{
+    public class BooksService
+    {
+        private List<Book> books;
+        public BooksService()
+        {
+            books = new List<Book>();
+            books.Add(new Book
+            {
+                Id = 1,
+                Title = "Atomic Habits",
+                Author = "Someone",
+                Language = "English"
+            });
+
+            books.Add(new Book
+            {
+                Id = 2,
+                Title = "Capra cu 3 iezi",
+                Author = "IDK",
+                Language = "Romanian"
+            });
+        }
+        public IEnumerable<Book> Get()
+        {
+            return books;
+        }
+
+        public Book Get(int id)
+        {
+            return books.Single(book => book.Id == id);
+        }
+
+        public void Post( Book value)
+        {
+            int id = books.Count + 1;
+            value.Id = id;
+            books.Add(value);
+        }
+
+        public void Put(int id, string value)
+        {
+
+        }
+
+        public void Delete(int id)
+        {
+            books.Remove(Get(id));
+        }
+    }
+}
