b82055c81377119ef504e3235487bb6d2d2c4c0d
Lucian Masca <lucianm0607@icloud.com>
2021-09-08 19:59:44 +0300

    Made the first test for the methods

---
+++ TestProject1/BooksServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using AspNetSandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestProject1
+{
+    public class BooksServiceTests
+    {
+        [Fact]
+        public void ConvertResponseToWeatherForecastTest()
+        {
+            // Assume
+            var booksService= new BooksService();
+
+
+            // Act
+            booksService.Post(new Book
+            {
+                Title = "Added book",
+                Author = "Someone",
+                Language = "English"
+            });
+            booksService.Delete(2);
+            booksService.Post(new Book
+            {
+                Title = "Added book 2",
+                Author = "Someone 2",
+                Language = "English 2"
+            });
+
+
+
+            // Assert
+            Assert.Equal("Added book 2", booksService.Get(3).Title);
+        }
+    }
+}
