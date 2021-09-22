b47419fcb2304c2c56ffc89a6a075914881f7adc
Lucian Masca <lucianm0607@icloud.com>
2021-09-22 08:36:53 +0300

    Fixed DTOs

--- AspNetSandbox2/DTOs/ReadBookDto.cs
+++ AspNetSandbox2/DTOs/ReadBookDto.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 
 namespace AspNetSandbox.DTOs
 {
-    public class CreateBookDto
+    public class ReadBookDto
     {
+        public int Id { get; set; }
+
         public string Title { get; set; }
 
         public string Author { get; set; }
---
+++ AspNetSandbox2/Data/DataTools.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetSandbox2.Data;
+using AspNetSandbox2.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetSandbox.Data
+{
+    public static class DataTools
+    {
+        public static void SeedData(this IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var applicationDbContext = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+                if (applicationDbContext.Book.Any())
+                {
+                    Console.WriteLine("The books are there.");
+                }
+                else
+                {
+                    applicationDbContext.Add(new Book
+                    {
+                        Id = 1,
+                        Title = "Harry Potter and the Goblet of Fire",
+                        Author = "JK Rowling",
+                        Language = "English",
+                        PurchasePrice = 50,
+                    });
+                    Console.WriteLine("Added book with title: Harry Potter and the Goblet of Fire");
+                    applicationDbContext.Add(new Book
+                    {
+                        Id = 2,
+                        Title = "The Happiest Man on Earth",
+                        Author = "Eddie Jaku",
+                        Language = "English",
+                        PurchasePrice = 35,
+                    });
+                    Console.WriteLine("Added book with title: The Happiest Man on Earth");
+                    applicationDbContext.SaveChanges();
+                }
+            }
+        }
+    }
+}
